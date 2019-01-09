2bb3938d9b06f4f816e2e119fed40474f76cb77c
Alejandra Castillo <ahle.castillo@gmail.com>
2019-01-09 18:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    for (var i = 0; i < instruments.length; i++) {
+
+      var musician = musicians[i];
+      var instrument = instruments[i];
+     
+     var newSentence = musician + " plays " + instrument; 
+      array.push(newSentence); 
+    }
+  }
+  return array; 
+}
+
+function johnLennonFacts(array) {
+  var newArray = [];
+  var i = 0; 
+
+  while (i < array.length) {
+    var fact = array[i];
+    var newSentence = fact + "!!!"; 
+    newArray.push(newSentence); 
+
+    i++; 
+  }
+  return newArray; 
+}
+
